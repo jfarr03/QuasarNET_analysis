1ee3f25c6ce7eef9074fab19d9df83b454a4d6ed
James Farr <james.a.farr@btopenworld.com>
2020-05-21 23:46:02 +0100

    Added nproc=32

--- prepare_batch/prepare_full_datasets.py
+++ prepare_batch/prepare_full_datasets.py
@@ -9,24 +9,24 @@ run_file = PREPDIR+'/run_batch/make_full_datasets.sh'
 run_file_text = '#!/usr/bin/env bash\n\n'
 
 run_file_text += '## Make truth file.\n'
-run_file_text += 'cmd="qn_parse_truth --sdrq {} --drq {} --out {}/data/truth/truth_dr12q.fits --mode BOSS"\n'.format(SDRQ,DRQ,OUTDIR)
+run_file_text += 'cmd="qn_parse_truth --sdrq {} --drq {} --out {}/data/truth/truth_dr12q.fits --mode BOSS --nproc 32"\n'.format(SDRQ,DRQ,OUTDIR)
 run_file_text += '$cmd\n\n'
 
 run_file_text += '## Make coadded dataset.\n'
-run_file_text += 'cmd="parse_data --spplates {}/*/spPlate-*.fits --spall {} --sdrq {} --out {}/data/coadd/full_datasets/data_dr12_coadd.fits"\n'.format(REDUXDIR,SPALL,SDRQ,OUTDIR)
+run_file_text += 'cmd="parse_data --spplates {}/*/spPlate-*.fits --spall {} --sdrq {} --out {}/data/coadd/full_datasets/data_dr12_coadd.fits --nproc 32"\n'.format(REDUXDIR,SPALL,SDRQ,OUTDIR)
 run_file_text += '$cmd\n\n'
 
 run_file_text += '## Make bestexp dataset.\n'
-run_file_text += 'cmd="parse_data --spplates {}/*/spPlate-*.fits --spall {} --sdrq {} --out {}/data/bestexp/full_datasets/data_dr12_bestexp.fits --use-best-exp"\n'.format(REDUXDIR,SPALL,SDRQ,OUTDIR)
+run_file_text += 'cmd="parse_data --spplates {}/*/spPlate-*.fits --spall {} --sdrq {} --out {}/data/bestexp/full_datasets/data_dr12_bestexp.fits --use-best-exp --nproc 32"\n'.format(REDUXDIR,SPALL,SDRQ,OUTDIR)
 run_file_text += '$cmd\n\n'
 
 run_file_text += '## Make randexp dataset.\n'
-run_file_text += 'cmd="parse_data --spplates {}/*/spPlate-*.fits --spall {} --sdrq {} --out {}/data/randexp/full_datasets/data_dr12_randexp.fits --use-random-exp --random-seed 0"\n'.format(REDUXDIR,SPALL,SDRQ,OUTDIR)
+run_file_text += 'cmd="parse_data --spplates {}/*/spPlate-*.fits --spall {} --sdrq {} --out {}/data/randexp/full_datasets/data_dr12_randexp.fits --use-random-exp --random-seed 0 --nproc 32"\n'.format(REDUXDIR,SPALL,SDRQ,OUTDIR)
 run_file_text += '$cmd\n\n'
 
 for dll in DLL_VALUES:
     run_file_text += '## Make coadded dataset with dll={}.\n'.format(dll)
-    run_file_text += 'cmd="parse_data --spplates {}/*/spPlate-*.fits --spall {} --sdrq {} --out {}/data/coadd/full_datasets/data_dr12_coadd_dll{}.fits --dll {}"\n'.format(REDUXDIR,SPALL,SDRQ,OUTDIR,dll,dll)
+    run_file_text += 'cmd="parse_data --spplates {}/*/spPlate-*.fits --spall {} --sdrq {} --out {}/data/coadd/full_datasets/data_dr12_coadd_dll{}.fits --dll {} --nproc 32"\n'.format(REDUXDIR,SPALL,SDRQ,OUTDIR,dll,dll)
     run_file_text += '$cmd\n\n'
 
 with open(run_file, 'w') as file:
