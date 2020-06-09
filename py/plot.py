8cd379c2128b51ae3536e57c9c142b3f9964d1d0
James Farr <james.a.farr@btopenworld.com>
2020-06-09 17:42:36 +0100

    Added normalisation

--- py/plot.py
+++ py/plot.py
@@ -339,6 +339,8 @@ def plot_qn_model_compare(data_table,strategies,filename=None,dv_max=6000.,nydec
         if norm_dvhist:
             hists = hists/(hists.sum(axis=1)[:,None])
         mean_hist = np.sum(hists,axis=0)
+        if norm_dvhist:
+            mean_hist = mean_hist/(mean_hist.sum(axis=1))
         if show_std:
             std_hist = np.std(hists,axis=0)
 
