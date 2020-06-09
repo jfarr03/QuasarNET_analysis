e8189a194139ab654d1a2f1894a8a6b897426563
James Farr <james.a.farr@btopenworld.com>
2020-06-09 17:53:50 +0100

    Fix

--- py/plot.py
+++ py/plot.py
@@ -339,9 +339,7 @@ def plot_qn_model_compare(data_table,strategies,filename=None,dv_max=6000.,nydec
         hists = np.vstack(hists)
         if norm_dvhist:
             hists = hists/((hists*(dv_mids.reshape(1,len(dv_mids)))).sum(axis=1)[:,None])
-        mean_hist = np.sum(hists,axis=0)
-        if norm_dvhist:
-            mean_hist = mean_hist/((mean_hist*dv_mids).sum())
+        mean_hist = np.mean(hists,axis=0)
         if show_std:
             std_hist = np.std(hists,axis=0)
 
