5ffd2cd7d675bd39c19bf929c397991921e9d6cf
James Farr <james.a.farr@btopenworld.com>
2020-06-09 16:34:40 +0100

    Minor change

--- py/plot.py
+++ py/plot.py
@@ -475,7 +475,7 @@ def plot_qn_model_data_compare(data_table,strategies,filename=None,dv_max=6000.,
     return fig, axs
 
 ## Function for Figure 4.
-def plot_reobservation_performance(data_table,strategies,filename=None,figsize=(12,6),eff_area=None,dv_max=6000.,zcut=2.1,ymin=0.94,xmin=47.,xmax=52.,verbose=False,n_highz_desi=50,nydec=0,filters=None,marker_size=100,strategies_to_plot=None,npoints_plot=None,point_shift=0.004,auto_legend=True,vmin=0.,vmax=1.,legend_loc=4,ncol=1,cbar_label=r'$c_{th}$',cbar_tick_mult=1.):
+def plot_reobservation_performance(data_table,strategies,filename=None,figsize=(12,6),eff_area=None,dv_max=6000.,zcut=2.1,ymin=0.94,xmin=47.,xmax=52.,verbose=False,n_highz_desi=50,nydec=0,filters=None,marker_size=100,strategies_to_plot=None,npoints_plot=None,point_shift=0.004,auto_legend=True,vmin=0.,vmax=1.,legend_loc=4,ncol=1,cbar_label=r'$c_{th}$',cbar_tick_mult=None):
 
     if filters is None:
         filters = {None: np.ones(len(data_table)).astype(bool)}
@@ -667,7 +667,8 @@ def plot_reobservation_performance(data_table,strategies,filename=None,figsize=(
         cb = fig.colorbar(points,label=cbar_label,cax=cbar_ax)
         cb.mappable.set_clim(vmin=vmin,vmax=vmax)
         cb.set_ticks(cb.get_ticks())
-        cb.set_ticklabels(cb.get_ticks()*cbar_tick_mult)
+        if cbar_tick_mult is not None:
+            cb.set_ticklabels(cb.get_ticks()*cbar_tick_mult)
 
     if filename is not None:
         plt.savefig(filename)
